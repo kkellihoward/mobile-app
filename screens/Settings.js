c18c1498a6c9f1c58f983b3c2ee84bb705ed1d14
Abdullah AL Hinaey <101839293+AbdullahALX@users.noreply.github.com>
2023-07-25 00:13:03 -0400

    changes on screens

---
+++ screens/Settings.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import {
+  Text,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+  View,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text
+        style={{
+          justifyContent: 'center',
+          textAlign: 'center',
+          color: 'black',
+        }}
+      >
+        Settings
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default Home;
